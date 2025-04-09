19548cb0194539653079d500efa150e40abd4c25
Karolvi25 <karolarana07@gmail.com>
2025-04-09 08:29:43 -0500

    Add files via upload

---
+++ condicional karol.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.estructuras_condicionales;
+
+/**
+ *
+ * @author karo villota
+ */
+public class Estructuras_condicionales {
+
+    public static void main(String[] args) {
+        String transporte = "";
+        if(transporte == "Automovil"){
+            System.out.println("preparar las cosas del maletero");
+            
+        }else if(transporte=="Motocicleta"){
+            System.out.println("preparar casco y capa de lluvia");
+            
+        }else{
+            System.out.println("ir caminando");
+        }
+    }
+}
